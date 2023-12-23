6cec314323c2177c0470e88070a36cbc2d40f332
TabindaNoor <tabindanoor415@gmail.com>
2023-12-24 00:19:15 +0500

    now working

--- src/App.js
+++ src/App.js
@@ -5,12 +5,7 @@ function App() {
   return (
     <div className="App">
       <div>
-        I am Tabinda Noor
-        ALLAHTALAH You know what i want 
-        meray mola meray pyare qadir 
-        meray malik mujy wo ata kr do me chahti hon
-        mujy kabhi bhi bhataknay na dena meray mola 
-        ay wo k jis k hath me meri jan ha 
+        
         </div>
     </div>
   );
